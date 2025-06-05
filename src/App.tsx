a8d516c030ca4d75f956488b19f1ef6afa9508eb
ayushsingh82 <ayushsinghmi711@gmail.com>
2025-06-05 14:46:42 +0530

    nouns added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import './App.css';
 
+const NOUNS_IMAGE_BASE = "https://noun.pics/";
+
 function App() {
+  const tokenIds = Array.from({ length: 9 }, (_, i) => 1300 + i);
+
   return (
     <div className="app">
       <div className="container">
-        {/* Main content will go here */}
+        <div className="nouns-container">
+          {tokenIds.map((tokenId) => (
+            <div key={tokenId} className="noun-image">
+              <img 
+                src={`${NOUNS_IMAGE_BASE}${tokenId}.png`}
+                alt={`NOUN ${tokenId}`}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
